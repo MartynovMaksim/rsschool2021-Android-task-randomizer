ab2dca67780b6bcc6424acb090006bb45231fc5c
MartynovMaksim <martynmaks@gmail.com>
2021-05-26 17:38:37 +0300

    Version 1.0

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,17 +1,28 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
 
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
+    private lateinit var communicator: Communicator
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        communicator = activity as Communicator
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,14 +38,40 @@ class FirstFragment : Fragment() {
         generateButton = view.findViewById(R.id.generate)
 
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
-        previousResult?.text = "Previous result: ${result.toString()}"
+//        previousResult?.text = "Previous result: ${result.toString()}"
+        previousResult?.text = getString(R.string.prev_result, result.toString())
 
-        // TODO: val min = ...
-        // TODO: val max = ...
+        var min: Int? = null
+        view.findViewById<EditText>(R.id.min_value).doAfterTextChanged {
+            view.findViewById<EditText>(R.id.min_value).text.toString().toIntOrNull()?.let {
+                min = it
+            }
+        }
+        var max: Int? = null
+        view.findViewById<EditText>(R.id.max_value).doAfterTextChanged {
+            view.findViewById<EditText>(R.id.max_value).text.toString().toIntOrNull()?.let {
+                max = it
+            }
+        }
+
 
         generateButton?.setOnClickListener {
-            // TODO: send min and max to the SecondFragment
+            if (isValidInfo(min, max)) {
+                min?.let { it1 -> max?.let { it2 -> communicator.openSecondFragment(it1, it2) } }
+            } else {
+                Toast.makeText(activity, R.string.toast_message, Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    private fun isValidInfo(min: Int?, max: Int?): Boolean {
+        return when {
+            min != null && max != null &&
+                    min < max && min <= Int.MAX_VALUE &&
+                    max <= Int.MAX_VALUE -> true
+            else -> false
+        }
+
     }
 
     companion object {
--- app/src/main/java/com/rsschool/android2021/SecondFragment.kt
+++ app/src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -1,17 +1,21 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 
 class SecondFragment : Fragment() {
 
     private var backButton: Button? = null
     private var result: TextView? = null
+    private var resultInt: Int = 0
+    private lateinit var communicator: Communicator
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,24 +25,36 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        communicator = activity as Communicator
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         result = view.findViewById(R.id.result)
         backButton = view.findViewById(R.id.back)
 
         val min = arguments?.getInt(MIN_VALUE_KEY) ?: 0
         val max = arguments?.getInt(MAX_VALUE_KEY) ?: 0
 
-        result?.text = generate(min, max).toString()
+        resultInt = generate(min, max)
+        result?.text = resultInt.toString()
 
         backButton?.setOnClickListener {
-            // TODO: implement back
+            communicator.openFirstFragment(resultInt)
+
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            communicator.openFirstFragment(resultInt)
         }
     }
 
+
     private fun generate(min: Int, max: Int): Int {
-        // TODO: generate random number
-        return 0
+        return (min..max).random()
     }
 
     companion object {
@@ -47,8 +63,10 @@ class SecondFragment : Fragment() {
         fun newInstance(min: Int, max: Int): SecondFragment {
             val fragment = SecondFragment()
             val args = Bundle()
+            args.putInt(MIN_VALUE_KEY, min)
+            args.putInt(MAX_VALUE_KEY, max)
+            fragment.arguments = args
 
-            // TODO: implement adding arguments
 
             return fragment
         }
